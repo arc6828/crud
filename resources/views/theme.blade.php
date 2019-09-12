2864559dad3b6dfb7136b3397da3376b7e30fc2e
arc6828 <chavalit.kow@gmail.com>
2019-09-12 12:26:24 +0700

    Ok

--- resources/views/theme.blade.php
+++ resources/views/theme.blade.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,7 +6,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Jekyll v3.8.5">
-    <title>Teacher</title>
+    <title> @yield("title") </title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/pricing/">
 
@@ -47,66 +46,16 @@
     </div>
 
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-      <h1 class="display-4">Teacher</h1>
+      <h1 class="display-4"> @yield("title") </h1>
       <p class="lead d-none">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It’s built with default Bootstrap components and utilities with little customization.</p>
     </div>
 
     <div class="container">
       <div class="text-center">
-        <table class="table table-striped">
-          <thead>
-            <tr>
-        			<th>#</th>
-              <th>ชื่อสกุล</th>
-              <th>รายละเอียด</th>
-        		</tr>
-          </thead>
-        	<tbody>
-            <tr>
-        			<td>1</td>
-        			<td>อาจารย์สุนี ปัญจะเทวคุปก์ </td>
-        			<td><a href="#">คลิก</a></td>
-        		</tr>
-            <tr>
-        			<td>2</td>
-        			<td>อาจารย์วิวัฒน์ ชินนาทศิริกุล์</td>
-        			<td><a href="#">คลิก</a></td>
-        		</tr>
-            <tr >
-        			<td>3 </td>
-        			<td>อาจารย์ดาวรถา วีระพันธ์</td>
-        			<td><a href="#">คลิก</a></td>
-        		</tr>
-            <tr>
-        			<td>4</td>
-        			<td>อาจารย์ณัฎฐิรา ศุขไพบูลย์์</td>
-        			<td><a href="#">คลิก</a></td>
-        		</tr>
-            <tr>
-        			<td>5 </td>
-              <td>อาจารย์ประณมกร อัมพรพรรดิ์</td>
-        			<td><a href="#">คลิก</a></td>
-        		</tr>
-            <tr>
-        			<td>6 </td>
-        			<td>อ่าจารย์วิศรุต ขวัญคุ้ม</td>
-        			<td><a href="#">คลิก</a></td>
-        		</tr>
-            <tr>
-        			<td>7 </td>
-        			<td>อาจารย์ณัฐรดี อนุพงค์ </td>
-        			<td><a href="#">คลิก</a></td>
-        		</tr>
-            <tr>
-        			<td>8 </td>
-        			<td>อาจารย์ชวลิต โควีระวงศ์</td>
-        			<td><a href="#">คลิก</a></td>
-        		</tr>
 
-        	</tbody>
-        </table>
+        @yield("content")
+
       </div>
-
       <footer class="pt-4 my-md-5 pt-md-5 border-top">
         <div class="row">
           <div class="col-12 col-md">
@@ -145,5 +94,5 @@
         </div>
       </footer>
     </div>
-  </body>
+</body>
 </html>

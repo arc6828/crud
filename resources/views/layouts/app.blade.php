27d1d785e4b1ae0cc23b9dd510e11f60e7d1cf29
arc6828 <chavalit.kow@gmail.com>
2019-10-06 16:25:47 +0700

    Ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,7 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-=======
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
->>>>>>> d393aa490b889b28cd06e6b86d8e10debc5f294f
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
@@ -16,15 +9,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-<<<<<<< HEAD
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-=======
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
@@ -38,16 +22,11 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
->>>>>>> d393aa490b889b28cd06e6b86d8e10debc5f294f
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-<<<<<<< HEAD
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-=======
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
->>>>>>> d393aa490b889b28cd06e6b86d8e10debc5f294f
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -60,25 +39,6 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-<<<<<<< HEAD
-                        @if(!Auth::check())
-                            <li><a class="nav-link" href="{{ url('/login') }}">Login</a></li>
-                            <li><a class="nav-link" href="{{ url('/register') }}">Register</a></li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ url('/logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
-
-                                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-=======
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -102,16 +62,11 @@
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
->>>>>>> d393aa490b889b28cd06e6b86d8e10debc5f294f
                                         @csrf
                                     </form>
                                 </div>
                             </li>
-<<<<<<< HEAD
-                        @endif
-=======
                         @endguest
->>>>>>> d393aa490b889b28cd06e6b86d8e10debc5f294f
                     </ul>
                 </div>
             </div>
@@ -121,11 +76,5 @@
             @yield('content')
         </main>
     </div>
-<<<<<<< HEAD
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-=======
->>>>>>> d393aa490b889b28cd06e6b86d8e10debc5f294f
 </body>
 </html>
